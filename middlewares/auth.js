f3e0469ad187cc39e2f2cb834cbda61adfa9f919
Aravindhan-K97 <aravindhan2825@gmail.com>
2025-03-18 22:54:09 +0530

    fixed

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,6 +2,8 @@ import { User } from "../models/userSchema.js";
 import { catchAsyncErrors } from "./catchAsyncError.js";
 import ErrorHandler from "./error.js";
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
   const { token } = req.cookies;
