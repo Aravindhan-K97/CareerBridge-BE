a28bdfcf75312a0a244fbe23a305a93917a80550
Aravindhan-K97 <aravindhan2825@gmail.com>
2025-03-18 22:52:15 +0530

    fixed

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,23 +2,15 @@ import { User } from "../models/userSchema.js";
 import { catchAsyncErrors } from "./catchAsyncError.js";
 import ErrorHandler from "./error.js";
 import jwt from "jsonwebtoken";
-import dotenv from "dotenv";
-dotenv.config();
 
 export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
-  // ✅ Check for cookies
-  const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
-
+  const { token } = req.cookies;
   if (!token) {
     return next(new ErrorHandler("User Not Authorized", 401));
   }
+  const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
 
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
-    req.user = await User.findById(decoded.id);
-    if (!req.user) return next(new ErrorHandler("User Not Found", 401));
-    next();
-  } catch (error) {
-    return next(new ErrorHandler("Invalid Token", 401));
-  }
+  req.user = await User.findById(decoded.id);
+
+  next();
 });
