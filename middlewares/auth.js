089976098bd0100d2fef74dab2c7d95c94edc431
Aravindhan-K97 <aravindhan2825@gmail.com>
2025-03-19 00:22:12 +0530

    fixed

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,12 +5,22 @@ import jwt from "jsonwebtoken";
 
 export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
   const { token } = req.cookies;
+
   if (!token) {
     return next(new ErrorHandler("User Not Authorized", 401));
   }
-  const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
 
-  req.user = await User.findById(decoded.id);
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    const user = await User.findById(decoded.id);
 
-  next();
+    if (!user) {
+      return next(new ErrorHandler("User no longer exists", 401));
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    return next(new ErrorHandler("Invalid or Expired Token", 401));
+  }
 });
